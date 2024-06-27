39d593fed6583f70844382c20dae6717c55702fc
david gustafson <git@qqrtfm.com>
2024-06-27 07:54:16 -0700

    bleh 2

--- scripts/player.gd
+++ scripts/player.gd
@@ -5,7 +5,7 @@ signal laser_shot(laser)
 @export var max_speed := 350.0
 @export var acceleration := 10.0
 @export var rotatation_speed := 500.0
-@export var laser_rof := 0.5
+@export var laser_rof := 0.1
 
 @onready var muzzle = $Muzzle
 @onready var lasers = $"../Lasers"
@@ -23,16 +23,13 @@ func _process(delta: float) -> void:
             shoot_cooldown = false
 
 func _physics_process(delta: float) -> void:
-    #var input_vector := Vector2(0, Input.get_axis("move_forward", "move_backward"))
     var input_vector := Vector2(0, 0)
     if Input.is_action_pressed("move_foreward"):
         print("move_forward")
-        #bleh
         input_vector.y = -1
     if Input.is_action_pressed("move_backward"):
         print("move_backward")
         input_vector.y = 1
-    print(input_vector)
     velocity += input_vector.rotated(rotation) * acceleration
     velocity = velocity.limit_length(max_speed)
 
@@ -41,8 +38,8 @@ func _physics_process(delta: float) -> void:
     if Input.is_action_pressed("rotate_left"):
         rotate(deg_to_rad( - rotatation_speed) * delta)
 
-    if input_vector.y == 0:
-        velocity = velocity.move_toward(Vector2.ZERO, 3)
+    #if input_vector.y == 0:
+    #    velocity = velocity.move_toward(Vector2.ZERO, 3)
     move_and_slide()
 
     var screen_size = get_viewport_rect().size
